9d517962a5a135e9c82ab28d076a9912531662c6
MiihauLOL <mrblitzlp@gmail.com>
2023-06-06 23:41:15 +0200

    I took great advice

--- MiihausCrowCurse/ModEntry.cs
+++ MiihausCrowCurse/ModEntry.cs
@@ -11,7 +11,8 @@ namespace MiihausCrowCurse
     internal sealed class ModEntry : Mod
     {
         private Random random;
-        
+        int nextSpawnTick = 0;
+
         public override void Entry(IModHelper helper)
         {
             random = new Random();
@@ -19,48 +20,40 @@ namespace MiihausCrowCurse
             helper.Events.GameLoop.UpdateTicked += OnUpdateTick;
         }
 
-
+        //randomness for the frequency
+        private bool ShouldSpawn()
+        {
+            if (Game1.ticks > nextSpawnTick)
+            {
+                nextSpawnTick = Game1.ticks + random.Next(120) + 100;
+                return true;
+            }
+            return false;
+        }
         private void OnUpdateTick(object sender, UpdateTickedEventArgs e)
         {
             // ignore if player hasn't loaded a save yet
             if (!Context.IsWorldReady)
                 return;
-            //randomness for the frequency
-            uint randomTime = Convert.ToUInt32(random.Next(120)+100);
-            if (e.IsMultipleOf(randomTime)) // Adjust the frequency by changing the number here
+            if (ShouldSpawn() == true) // Adjust the frequency by changing the number here
             {
                 if (Game1.currentLocation.IsOutdoors == true) { 
                     //this.Monitor.Log("Trying to spawn...", LogLevel.Debug);
                     // Get a random X and Y coordinate within the farm area
                     int randomX;
                     int randomY;
-                    double randomPlusMinus = random.NextDouble();
-
-                    if (randomPlusMinus>0.5)
-                    {
-                        randomX = Game1.player.getTileX() + random.Next(5);
-                    }
-                    else { randomX = Game1.player.getTileX() - random.Next(5); }
-
-                    randomPlusMinus = random.NextDouble();
-                    if (randomPlusMinus > 0.5)
-                    {
-                        randomY = Game1.player.getTileY() + random.Next(8);
-                    }
-                    else { randomY = Game1.player.getTileY() - random.Next(8); }
-                    
+                    randomX = Game1.player.getTileX() + random.Next(-5, 5);
+                    randomY = Game1.player.getTileY() + random.Next(-5, 5);
                     //this.Monitor.Log("made Random number X: " + randomX + ", and Y: " + randomY + ".", LogLevel.Debug);
                     // Spawn a crow at the random location
-                    if (Game1.currentLocation.isWaterTile(randomX, randomY).Equals(false)) {
+                    if (Game1.currentLocation.isWaterTile(randomX, randomY).Equals(false)) 
+                    {
                         Game1.playSound("coin");
                         Game1.currentLocation.critters.Add(new Crow(randomX, randomY));
                         //this.Monitor.Log("Spawing crow...", LogLevel.Debug);
-
                     }
                 }
             }
-
         }
-
     }
 }

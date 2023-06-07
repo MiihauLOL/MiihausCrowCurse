c8efa617158a30a0a1cc1d7d8659ef3bbc165537
MiihauLOL <mrblitzlp@gmail.com>
2023-06-07 02:36:00 +0200

    version 1.1.0

--- MiihausCrowCurse/ModConfig.cs
+++ MiihausCrowCurse/ModConfig.cs
@@ -1,6 +1,6 @@
 ﻿public sealed class ModConfig
 {
-    public int SpawnFreqencyInSeconds { get; set; } = 2;
+    public int SpawnFreqencyInSeconds { get; set; } = 2; //comment test
     public int SpawnRadiusAroundFarmer { get; set; } = 5;
     public bool MakeSpawnSound { get; set; } = true;
 }
